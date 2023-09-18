7e46b637928d2fadaf084bf71589b0644d9ba723
Martin Nolan <martinnolan_1@live.co.uk>
2023-09-18 19:16:40 +0100

    added:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import InputEvents from "./events/InputEvents";
+import MovementEvents from "./events/MovementEvents";
+import "./events/Events.css";
+import Card from "./Card.js";
+import "./Card.css";
+import FormValidation from "./FormValidation";
+import ShoppingList from "./ShoppingList";
+import "./ShoppingList.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card />
+      <InputEvents />
+      <MovementEvents />
+      <FormValidation />
+      <ShoppingList />
     </div>
   );
 }
